c962158be9a3becc520e14e1349f5a0f4f6a3e52
Eduardo Henrique da Silva <edu.slhenrique@gmail.com>
2021-08-29 05:34:27 -0300

    # Add connect of static page

--- src/main/java/br/com/eduardo/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/eduardo/brasileiraoapi/util/ScrapingUtil.java
@@ -1,12 +1,35 @@
 package br.com.eduardo.brasileiraoapi.util;
 
 import br.com.eduardo.brasileiraoapi.dto.PartidaGoogleDTO;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
 
 public class ScrapingUtil {
     private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
     private static final String COMPLEMENTO = "&hl=pt-BR";
-    public static void main(String[] args){
-        PartidaGoogleDTO partida = new PartidaGoogleDTO();
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
 
+    public static void main(String[] args) {
+        String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO;
+        ScrapingUtil scraping = new ScrapingUtil();
+        scraping.obterInformacoesPartida(url);
+    }
+
+    public static PartidaGoogleDTO obterInformacoesPartida(String url) {
+        PartidaGoogleDTO partida = new PartidaGoogleDTO();
+        Document document;
+
+        try {
+            document = Jsoup.connect(url).get();
+            String title = document.title();
+            LOGGER.info(title);
+        } catch (IOException e) {
+            LOGGER.error("Erro ao conectar no google com JSOUP -> {}", e.getMessage());
+        }
+
+        return partida;
     }
 }
