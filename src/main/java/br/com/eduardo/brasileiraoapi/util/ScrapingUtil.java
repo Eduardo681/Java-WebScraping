e00dcfa0f37319b3a676cb9494c8e769e05e7956
Eduardo Henrique da Silva <edu.slhenrique@gmail.com>
2021-08-29 05:15:36 -0300

    # Add Enum and Create Main Class

---
+++ src/main/java/br/com/eduardo/brasileiraoapi/util/ScrapingUtil.java
@@ -0,0 +1,9 @@
+package br.com.eduardo.brasileiraoapi.util;
+
+public class ScrapingUtil {
+    private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
+    private static final String COMPLEMENTO = "&hl=pt-BR";
+    public static void main(String[] args){
+
+    }
+}
